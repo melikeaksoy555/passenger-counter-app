ec42d861cfa777ead1b4133cb455ab26034f8221
melikeaksoy555 <melikeficici@hotmail.com>
2024-08-02 18:02:48 +0300

    button id="increment-btn" ekleme ve button style belirleme

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // document.getElementById("counter-el").innerHTML = 5;
 
-let ilkSayi = 5; // değiştirilebilir değişkenler için let kullanılır. 
-
-const ikinciSayi = 10;
-
-ilkSayi = 25;
-
-console.log(ilkSayi);
