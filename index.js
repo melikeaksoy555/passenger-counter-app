9b058ae3a15ecee43e663768bbe466b199a2b626
melikeaksoy555 <melikeficici@hotmail.com>
2024-08-02 18:58:25 +0300

    js dosyasında function increment(), ilkSayı +=6, console.log(ilkSayı)

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // document.getElementById("counter-el").innerHTML = 5;
 
+let ilkSayı = 5;
+
+function increment(){
+    ilkSayı += 6;
+    console.log(ilkSayı);
+
+}
+increment();
+increment();
+increment();
+increment();
